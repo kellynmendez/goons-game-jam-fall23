f1a1a488a86b009b1bee51d566747289bc77b3b3
oleksandra_dzhura_utd <oxd220008@utdallas.edu>
2023-09-28 15:25:26 -0500

    Timer (+brought back some UI changes)

--- Assets/Game/Scripts/UI/MainMenuButtons.cs
+++ Assets/Game/Scripts/UI/MainMenuButtons.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuButtonPress : MonoBehaviour
+public class MainMenuButtons : MonoBehaviour
 {
     // Enable the play button
     public void PressPlayButton()
--- Assets/Game/Scripts/UI/SceneManager.cs
+++ Assets/Game/Scripts/UI/SceneManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (SceneManager.GetActiveScene().name == "Controls")
+            {
+                SceneManager.LoadScene("MainLevel");
+            }
+            // SceneManager.GetActiveScene().name == "MainMenu"
+            if (SceneManager.GetActiveScene().name == "Credits")
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
     }
 }
