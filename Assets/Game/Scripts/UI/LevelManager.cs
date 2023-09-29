4c6236336b31f233206e03eb04a40fbf598b2b89
Kellyn Mendez <kpm.mendez@gmail.com>
2023-09-28 22:54:06 -0500

    update score set up

--- Assets/Game/Scripts/UI/LevelManager.cs
+++ Assets/Game/Scripts/UI/LevelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
